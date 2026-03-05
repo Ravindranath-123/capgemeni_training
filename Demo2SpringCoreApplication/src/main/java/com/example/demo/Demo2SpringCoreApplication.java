c19878f4ba5a81c25b5c4e39a25f3986899ee664
Ravindranath Potturu <119026148+Ravindranath-123@users.noreply.github.com>
2026-03-05 16:50:11 +0530

    Spring Core Tasks

---
+++ Demo2SpringCoreApplication/src/main/java/com/example/demo/Demo2SpringCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2SpringCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2SpringCoreApplication.class, args);
+	}
+
+}
